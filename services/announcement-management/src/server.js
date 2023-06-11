2bb3d0496202e4c3a913fc91b954f3a4815ace6a
Bohdan Mykhayliv <bohdan.mykhayliv@bohdanmykhayliv.local>
2023-06-11 12:41:44 +0300

    Add services registration in Consul

--- services/announcement-management/src/server.js
+++ services/announcement-management/src/server.js
@@ -2,12 +2,16 @@ import express from 'express';
 
 import { announcementRouter } from './routes/announcement.js';
 import { getMysqlConfig, setMysqlConnection } from './persistence/mysql/mysql.js';
+import { registerSelf } from './utils/consul/consul.js';
 
 const app = express();
 const port = 3001
 
 app.use(express.json())
 app.use(announcementRouter);
+
+// Register service in Consul
+await registerSelf('announcement-management', 'localhost', port)
 
 const mysqlConfig = getMysqlConfig();
 
